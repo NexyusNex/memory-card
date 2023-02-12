a7f40f3024ef4c9f3e57ce8bd050fab2222e62df
NexyusNex <shootdietale@gmail.com>
2023-02-12 16:38:13 +0100

    Images are now displayed from the array with a function

--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import BanzaiBill from "../images/32px-SMW_Banzai_Bill_Sprite.png";
 import Wiggler from "../images/32px-SMW_Wiggler_Sprite.png";
 import Blurp from "../images/SMW_Blurp_Sprite.png";
@@ -13,74 +12,66 @@ import Pokey from "../images/SMW_Pokey_Sprite.png";
 import Rex from "../images/SMW_Rex_Sprite.png";
 
 export default function Gameboard() {
-  const [cards, setCards] = useState({
-    0: {
+  const cards = [
+    {
       src: BanzaiBill,
       name: "Banzai bill",
     },
-    1: {
+    {
       src: Wiggler,
       name: "Wiggler",
     },
-    2: {
+    {
       src: Blurp,
       name: "Blurp",
     },
-    3: {
+    {
       src: Boo,
       name: "Boo",
     },
-    4: {
+    {
       src: ClappinChuck,
       name: "Clappin' Chuck",
     },
-    5: {
+    {
       src: DinoRhino,
       name: "Dino Rhino",
     },
-    6: {
+    {
       src: Koopa,
       name: "Koopa",
     },
-    7: {
+    {
       src: Magikoopa,
       name: "Magikoopa",
     },
-    8: {
+    {
       src: MontyMole,
       name: "Monty Mole",
     },
-    9: {
+    {
       src: Pokey,
       name: "Pokey",
     },
-    10: {
+    {
       src: Rex,
       name: "Rex",
     },
-    11: {
+    {
       src: Paratroopa,
       name: "Paratroopa",
     },
-  });
-
-  return (
-    <div className="card-container">
-      <div className="card">
-        <img src={cards[0].src} alt="banzai bill"></img>
-        <h2>{cards[0].name}</h2>
-      </div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-    </div>
-  );
+  ];
+  function DisplayCards() {
+    const list = cards.map((element) => {
+      return (
+        <div className="card">
+          <img src={element.src} alt={element.name}></img>
+          <p>{element.name}</p>
+        </div>
+      );
+    });
+    return <div className="card-container">{list}</div>;
+  }
+  return <DisplayCards cards={cards}></DisplayCards>;
 }
