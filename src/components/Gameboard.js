94a37db38a7c7942e9e936386338803661833ef3
NexyusNex <shootdietale@gmail.com>
2023-02-12 15:53:55 +0100

    Imported images and added their properties to an array

--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -1,11 +1,66 @@
 import React, { useState } from "react";
 import BanzaiBill from "../images/32px-SMW_Banzai_Bill_Sprite.png";
+import Wiggler from "../images/32px-SMW_Wiggler_Sprite.png";
+import Blurp from "../images/SMW_Blurp_Sprite.png";
+import Boo from "../images/SMW_Boo_Sprite.png";
+import ClappinChuck from "../images/SMW_Clappin_Chuck_Sprite.png";
+import DinoRhino from "../images/SMW_Dino_Rhino_Sprite.png";
+import Koopa from "../images/SMW_Koopa.png";
+import Magikoopa from "../images/SMW_Magikoopa_Sprite.png";
+import MontyMole from "../images/SMW_Monty_Mole_Sprite.png";
+import Paratroopa from "../images/SMW_Paratroopa_Sprite.png";
+import Pokey from "../images/SMW_Pokey_Sprite.png";
+import Rex from "../images/SMW_Rex_Sprite.png";
 
 export default function Gameboard() {
   const [cards, setCards] = useState({
     0: {
-      name: "Banzai bill",
       src: BanzaiBill,
+      name: "Banzai bill",
+    },
+    1: {
+      src: Wiggler,
+      name: "Wiggler",
+    },
+    2: {
+      src: Blurp,
+      name: "Blurp",
+    },
+    3: {
+      src: Boo,
+      name: "Boo",
+    },
+    4: {
+      src: ClappinChuck,
+      name: "Clappin' Chuck",
+    },
+    5: {
+      src: DinoRhino,
+      name: "Dino Rhino",
+    },
+    6: {
+      src: Koopa,
+      name: "Koopa",
+    },
+    7: {
+      src: Magikoopa,
+      name: "Magikoopa",
+    },
+    8: {
+      src: MontyMole,
+      name: "Monty Mole",
+    },
+    9: {
+      src: Pokey,
+      name: "Pokey",
+    },
+    10: {
+      src: Rex,
+      name: "Rex",
+    },
+    11: {
+      src: Paratroopa,
+      name: "Paratroopa",
     },
   });
 
