a7d96411d9acb7187b70fa025f92b1ce630cf4b9
NexyusNex <shootdietale@gmail.com>
2023-02-12 14:34:46 +0100

    Made a header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
+import Header from "./components/Header";
+
 function App() {
   return (
     <div className="container">
-      <div className="header">
-        <h1>Memory Card</h1>
-      </div>
+      <Header></Header>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+export default function Header(props) {
+  return (
+    <div className="header">
+      <h1>Memory Card</h1>
+      <div>
+        <p>Score:</p>
+        <p>Max score:</p>
+      </div>
+    </div>
+  );
+}
